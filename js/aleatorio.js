d864c4570dc61a777c7a8600e427778e2d4e672f
ProfeEmanuel <140930070+ProfeEmanuel@users.noreply.github.com>
2025-09-01 02:06:36 +0000

    missao nomes aleatorios

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,7 @@
+const nomes = ["Emanuel", "Michele", "Alfred", "Ramona", "Aurora", "Alice", "Panterinha", "Branquinha", "Bebe"];
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+import{aleatorio, nome} from './aleatorio.js'
+import{perguntas} from './perguntas.js'
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -36,11 +40,25 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
 
+function substituiNome (){
+    for (const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
 
+substituiNome();
 mostraPergunta();
